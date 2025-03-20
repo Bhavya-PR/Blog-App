239028f58f4ac3477997cdbf25da35e1ee654af5
Bhavya-PR <bhavyapandurangan494@gmail.com>
2025-03-20 18:51:02 +0530

    updated

--- project1/blog/views.py
+++ project1/blog/views.py
@@ -4,10 +4,10 @@ from django.urls import reverse
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    return render(request,'blog/index.html')
 
 def detail(request,post_id):
-    return HttpResponse(f"You are viewing the post detail page and the Id is {post_id}")
+    return render(request,'blog/detail.html')
 
 def old_url_redirect(request):
     return redirect(reverse('blog:new_page_url'))
